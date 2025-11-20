255623ecc5ad5ba83e01f3ec8ae3f6dd36b7a40a
Joni Korpela <joni.korpela@student.oulu.fi>
2025-11-20 13:49:09 +0200

    paskaa

--- Design_Task_3/CORDIC_3.m
+++ Design_Task_3/CORDIC_3.m
@@ -1,104 +1,85 @@
-clc;
-clear;
+function pass = CORDIC_3(x, y, N_iter)
 
-%group 2 params for task 2
-phi_0 = 8.0;     % Base angle [deg] 
-N = 10;          % Number of angles
-d_max = 1.0e-3;  % Maximum error
+pass = true;
 
-% angle step
-delta_phi = 360 / N;
+phi_deg = -33.75;
+phi = deg2rad(phi_deg);
 
-% # of iterations, set arbitrarily
-N_iter = 11;
+% reduce to +-pi range for correct initialization
+phi = mod(phi + pi, 2*pi) - pi;
+% initialize to +-pi/2 range
+if phi < 0
+    dinit = -1;
+else
+    dinit = 1;
+end 
 
-cos_vals = zeros(1,N);
-sin_vals = zeros(1,N);
-cos_vals_cmp = zeros(1,N);
-sin_vals_cmp = zeros(1,N);
+% vector to rotate
+x_orig = x;
+y_orig = y;
 
-for n = 0:N-1
-    % phi_deg = phi_0 + n*delta_phi;
-    % phi = deg2rad(phi_deg)
+x0 = -dinit * y_orig;
+y0 = dinit * x_orig;
+z0 = phi - dinit * pi/2;
 
-    % phi_deg = -33.75;
-    phi_deg = 100;
-    phi = deg2rad(phi_deg);
+x = x0;
+y = y0;
+z = z0;
 
-    % reduce to +-pi range for correct initialization
-    phi = mod(phi + pi, 2*pi) - pi;
-    % initialize to +-pi/2 range
-    if phi < 0
-        dinit = -1;
+% array for saving rotation dirs
+d_arr = zeros(1, N_iter);
+
+for k = 0:N_iter-1
+
+    % determine the rotation dir that gets us closest
+    angles = [atan(2^(-k)), -atan(2^(-k)), 0];
+    [~, idx] = min( abs( z - angles ) );
+    if idx == 1
+        d = 1;
+    elseif idx == 2
+        d = -1;
     else
-        dinit = 1;
-    end 
-
-    x = 1;
-    y = 0;
-
-    x0 = -dinit * y;
-    y0 = dinit * x;
-    z0 = phi - dinit * pi/2;
-
-    x = x0;
-    y = y0;
-    z = z0;
-
-    % array for saving rotation dirs
-    d_arr = zeros(1, N_iter);
-
-    for k = 0:N_iter-1
-        if z < 0
-            if z + atan(2^(-k)) > 0
-                d = 0;       % don’t rotate
-            else
-                d = -1;      % rotate negatively
-            end
-        else
-            if z - atan(2^(-k)) < 0
-                d = 0;       % don’t rotate
-            else
-                d = 1;       % rotate positively
-            end
-
-        end
-
-
-        % store rotation into aray for gain computation
-        d_arr(k + 1) = d;
-
-        nx = x - d * y * 2^(-k);
-        ny = y + d * x * 2^(-k);
-        nz = z - d * atan(2^(-k));
-        x = nx;
-        y = ny;
-        z = nz;
+        d = 0;
     end
 
-    % CORDIC gain
-    A_N = 1;
-    for k = 0:N_iter-1
-        if d_arr(k+1) ~= 0
-            A_N = A_N * sqrt(1 + 2^(-2*k));
-        end
-    end
-    x = x / A_N ;
-    y = y / A_N ;
+    % store rotation into aray for gain computation
+    d_arr(k + 1) = d;
 
-    cos_vals(n+1) = x;
-    sin_vals(n+1) = y;
-    cos_vals_cmp(n+1) = cosd(phi_deg);
-    sin_vals_cmp(n+1) = sind(phi_deg);
+    nx = x - d * y * 2^(-k);
+    ny = y + d * x * 2^(-k);
+    nz = z - d * atan(2^(-k));
+    x = nx;
+    y = ny;
+    z = nz;
 end
-disp('Rotation directions:'); disp(d_arr)
-disp('cos (CORDIC):'); disp(cos_vals)
-disp('cos (true) :'); disp(cos_vals_cmp)
-disp('sin (CORDIC):'); disp(sin_vals)
-disp('sin (true) :'); disp(sin_vals_cmp)
 
-max_error = max(max(abs(cos_vals_cmp - cos_vals)), max(abs(sin_vals_cmp - sin_vals)))
-if max_error >= d_max
+% CORDIC gain
+A_N = 1;
+for k = 0:N_iter-1
+    if d_arr(k+1) ~= 0
+        A_N = A_N * sqrt(1 + 2^(-2*k));
+    end
+end
+x = x / A_N ;
+y = y / A_N ;
+
+% store true and computed vector values
+true_val_x = x_orig * cos(phi) - y_orig * sin(phi);
+true_val_y = y_orig * cos(phi) + x_orig * sin(phi);
+
+% compute error
+ang_true = atan2(true_val_y, true_val_x);
+ang_cordic = atan2(y, x);
+ang_error = rad2deg(abs(ang_true - ang_cordic));
+
+% display rotation directions
+disp('Rotation directions:'); disp(d_arr)
+
+
+error_max = 0.2; %deg
+disp('Maximum angular error:'); disp(ang_error);
+if max(ang_error) >= error_max
     fprintf('Maximum error exceeded!\n');
+    pass = false;
 end
 
